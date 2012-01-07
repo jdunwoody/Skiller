7d1a6e11679d309a9ec3cc59c55eecbfd2f8c937
James Dunwoody <jamesdunwoody@gmail.com>
2012-01-07 23:15:23 +1100

    Created list activity with load filesystem datastore

---
+++ src/com/james/skiller/DataLoader.java
@@ -0,0 +1,27 @@
+package com.james.skiller;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.Context;
+
+public class DataLoader {
+	static String FILENAME = "hello_file";
+	String string = "hello world!";
+
+	public static String[] load(Context context) throws IOException {
+		FileInputStream fis = context.openFileInput(FILENAME);
+		byte[] data = new byte[1024];
+		int numBytes = fis.read(data);
+		fis.close();
+		return new String[] {};
+	}
+
+	public void save(Context context) throws IOException {
+		FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+		fos.write(string.getBytes());
+		fos.close();
+	}
+}
---
+++ src/com/james/skiller/model/SkillTree.java
@@ -0,0 +1,9 @@
+package com.james.skiller.model;
+
+public class SkillTree {
+	private final String row;
+
+	public SkillTree(String row) {
+		this.row = row;
+	}
+}
